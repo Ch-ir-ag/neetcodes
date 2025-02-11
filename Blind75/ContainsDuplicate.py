b493f258913440ad3f94cf1fbe7bf36ee9355eea
Ch-ir-ag <csjoshi2003@gmail.com>
2025-02-11 22:27:25 +0000

    Completed Contains Duplicate problem

--- Blind75/ContainsDuplicate.py
+++ Blind75/ContainsDuplicate.py
@@ -1,1 +1,6 @@
-print("hi!")
+#TODO Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
+
